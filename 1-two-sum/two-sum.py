0679d5b28dfca095c9953b373849e3b1aeb52369
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2025-04-25 12:12:31 +0530

    Time: 3 ms (60.29%) | Memory: 13.4 MB (31.43%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def twoSum(self, nums, target):
         n=len(nums)
-        for i in range(0,n+1):
-            for j in range(i+1,n):
-                if(nums[i]+nums[j]==target):
-                    return [i,j]
+        mpp={}
+        for i in range(n):
+            number=nums[i]
+            x=target-number
+            if x not in mpp:
+                mpp[number]=i
+            else:
+                return [mpp[x],i]
+        return [-1,-1]
+        
         """
         :type nums: List[int]
         :type target: int
