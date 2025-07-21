c37e85d71df708b38f11484198d1945f139b13fa
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2025-07-21 12:56:33 +0530

    Time: 2 ms (95.18%) | Memory: 12.5 MB (51.68%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,10 +4,22 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        numstr=str(x)
+        '''numstr=str(x)
         rev_string=numstr[ : :-1]
         if(numstr==rev_string):
             return True
         else:
+            return False'''
+        
+        if(x<0):
             return False
+
+        rev=0
+        original=x
+        while(x!=0):
+            lastdigit=x%10
+            rev=(rev*10)+lastdigit
+            x=x//10
+        return(original==rev)
+            
         
