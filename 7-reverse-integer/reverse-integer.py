36745ea091ce5b72a76c94b1308dacf0e9e1b30a
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2025-07-21 11:29:48 +0530

    Time: 20 ms (42.98%) | Memory: 12.5 MB (7.39%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        INT_MIN,INT_MAX=-2**31,2**31+1
+        INT_MIN,INT_MAX=-2**31,2**31-1
         sign=-1 if x<0 else 1
         x=abs(x)
         rev=0
