489b21ac9780020a7e6ca46f0f6f5ca766dc6957
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2026-01-22 13:38:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Vowel or Not/vowel-or-not.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+    def isVowel (ob,c):
+        # code here 
+        Set=['a','A','e','E','i','I','o','O','u','U']
+        if c in Set:
+            return "YES"
+        return "NO"
