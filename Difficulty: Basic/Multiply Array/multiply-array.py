2b9fceb10d83233bbf8eba7d14c326822b6baf27
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2026-01-28 22:33:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiply Array/multiply-array.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def longest(self, arr, n):
+        #Code Here
+        res=1
+        for i in arr:
+            res=res*i
+        return res
+    
