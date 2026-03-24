b33dfb016d510b2689dacfa1df9f96a23b464b35
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2026-03-24 18:59:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiplication Table/multiplication-table.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+class Solution:
+    def getTable(self,n):
+        # code here
+        res=[]
+        for i in range(1,11):
+            ans=i*n
+            res.append(ans)
+        return res
+        
+            
+            
