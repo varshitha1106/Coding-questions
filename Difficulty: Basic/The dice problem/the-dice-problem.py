ee20f284469c181fd1fa6cd9c45511a854af02d3
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2026-04-18 16:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The dice problem/the-dice-problem.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def oppositeFaceOfDice(self, n):
+    	#code here 
+    	for i in range(1,7):
+    	    if n+i==7:
+    	        return i
+    	       
