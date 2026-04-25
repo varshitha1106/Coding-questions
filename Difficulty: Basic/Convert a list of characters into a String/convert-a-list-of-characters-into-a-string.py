0205188bdceac282b2b7f92240a0ab0030cc26db
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2026-04-25 17:28:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert a list of characters into a String/convert-a-list-of-characters-into-a-string.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def chartostr(self, arr,N):
+        # code here
+        res=""
+        for i in arr:
+            res+=i
+        return res
+        
